7599d270adfafe4c8910399645041d2aacaa370e
Tsing <wq123@mail.ustc.edu.cn>
2020-07-11 21:17:58 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+var my_json = [
+{
+	"website_name": "网络通",
+	"website_link": "http://192.168.170.6",
+	"website_single_text": "网"
+},
+{
+	"website_name": "多吉搜索",
+	"website_link": "https://www.dogedoge.com",
+	"website_single_text": "搜"
+},
+{
+	"website_name": "谷歌镜像",
+	"website_link": "https://www.library.ac.cn",
+	"website_single_text": "谷"
+},
+{
+	"website_name": "Yandex",
+	"website_link": "https://yandex.com",
+	"website_single_text": "Y"
+},
+{
+	"website_name": "GitHub",
+	"website_link": "https://github.com",
+	"website_single_text": "G"
+},
+{
+	"website_name": "油猴脚本",
+	"website_link": "https://greasyfork.org/zh-CN",
+	"website_single_text": "油"
+},
+{
+	"website_name": "吾爱破解",
+	"website_link": "https://www.52pojie.cn",
+	"website_single_text": "吾"
+},
+{
+	"website_name": "V2EX",
+	"website_link": "https://www.v2ex.com",
+	"website_single_text": "V"
+},
+{
+	"website_name": "测速网",
+	"website_link": "https://www.speedtest.cn",
+	"website_single_text": "测"
+},
+{
+	"website_name": "历史价格查询",
+	"website_link": "http://tool.manmanbuy.com/HistoryLowest.aspx",
+	"website_single_text": "历"
+},
+{
+	"website_name": "小众软件",
+	"website_link": "https://www.appinn.com/",
+	"website_single_text": "小"
+},
+]
